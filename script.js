c4edfc78cd79ea98b36b5001b90ea3dc77846cb0
Roberto Coral <33666303+robertocoral@users.noreply.github.com>
2022-02-07 12:48:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
-require(["esri/views/MapView", "esri/WebMap"], (MapView, WebMap) => {
-        const webmap = new WebMap({
-          portalItem: {
-            // autocasts as new PortalItem()
-            id: "bf8e7c0dc6024742a55e4ab09fd1db36"
-          }
+require([
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/layers/ImageryLayer",
+        "esri/layers/support/RasterFunction"
+      ], (Map, MapView, ImageryLayer, RasterFunction) => {
+
+        const imagePopupTemplate = {
+          // autocasts as new PopupTemplate()
+          title: "Data from {SensorName} satellite",
+          content: `
+            Rendered RGB values: <b>{Raster.ServicePixelValue} </b>
+            <br>Original values (Coastal, B, G, R, NIR, SWIR1, SWIR2, Panchromatic, Cirrus, TIRS1, TIRS2): <b>{Raster.ItemPixelValue} </b>
+            `
+        };
+
+        const serviceRFT = new RasterFunction({
+          functionName: "NDVI Colorized",
+          variableName: "Raster"
         });
+
+        const layer = new ImageryLayer({
+          url: "https://landsat2.arcgis.com/arcgis/rest/services/Landsat8_Views/ImageServer",
+          renderingRule: serviceRFT,
+          popupTemplate: imagePopupTemplate
+        });
+
+        const map = new Map({
+          basemap: "hybrid",
+          layers: [layer]
+        });
+
         const view = new MapView({
-          map: webmap,
-          container: "viewDiv"
+          container: "viewDiv",
+          map: map,
+          center: [-90.19973698105836, 38.625784113754406],
+          zoom: 14,
+          popup: {
+            actions: []
+          }
         });
       });
